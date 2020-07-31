58b2fc18f080bdc31c51d6b4648c19623e6ceb4d
Kevin Li <kevinli29033@gmail.com>
2020-07-30 17:01:34 -0700

    added uikit example project setup

---
+++ UIKitExample/UIKitExample/ViewController.swift
@@ -0,0 +1,14 @@
+// Kevin Li - 4:58 PM - 7/30/20
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
